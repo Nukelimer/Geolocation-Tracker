b79d10a7c3e918d4886ebf9d8d2cbed421d3d6e9
codewithremi <codewithremi@gmail.com>
2023-08-13 12:25:42 +0100

    removed sections of the logic.

--- asset/main.js
+++ asset/main.js
@@ -7,20 +7,16 @@ const refresh = document.querySelector('.refresh');
 
 navigator.geolocation.watchPosition(
   (responses) => {
-    if (!responses.coords.speed) {
-      speed.textContent = 0;
-    } else {
+   
       modal.classList('hide');
       speed.textContent = responses.coords.speed;
       compass.style.transform = `rotate(${responses.coords.heading}deg)`;
       console.log(responses.coords.heading);
-    }
+ 
   },
   (err) => {
-    if (err.code == 1) {
-      parent.classList.add('hide');
-      modal.classList.add('show');
-    }
+    parent.classList.add('hide');
+    modal.classList.add('show');
   }
 );
 
